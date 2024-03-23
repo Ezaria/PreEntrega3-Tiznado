12aca538590e53c2c9fbbf0f611770fb9fef6181
Ezaria <felipetiznado1@gmail.com>
2024-03-22 21:09:49 -0300

    probando cosas

--- src/main.js
+++ src/main.js
@@ -58,6 +58,7 @@ function crearProducto() {
 
       const boton = document.createElement('button');
       boton.classList.add('buttonProducto');
+      
       boton.textContent = 'Agregar';
       boton.setAttribute("onclick",`agregar(${producto.id})`);
 
@@ -111,25 +112,30 @@ function calcular() {
 calcular();
 
 
-const tost = document.getElementById('toastify')
 
 
-tost.addEventListener('click', () => {
-  Toastify({
-  text: "Producto Agregado",
-  duration: 800,
-  destination: "https://github.com/apvarun/toastify-js",
-  newWindow: true,
-  close: true,
-  gravity: "bottom", // `top` or `bottom`
-  position: "right", // `left`, `center` or `right`
-  stopOnFocus: true, // Prevents dismissing of toast on hover
-  style: {
-    background: "linear-gradient(to right, #00b09b, #96c93d)",
-  },
-  onClick: function(){} // Callback after click
-  }).showToast(); 
-})
+for (var i = 0; i < 1; i++ ) {
+
+  const tost = document.getElementById('toastify')
+  tost.addEventListener('click', () => {
+    Toastify({
+    text: "Producto Agregado",
+    duration: 800,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "bottom", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function(){} // Callback after click
+    }).showToast(); 
+  })
+  
+  
+  tost.textContent ="hola que hace"
 
 
-tost.textContent ="hola que hace"
+}
